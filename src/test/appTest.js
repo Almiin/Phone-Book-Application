8409784d398380333947d763e424603a07165129
Almiin <almin.pupalovic1@gmail.com>
2019-06-13 04:09:34 +0200

    first commit

--- src/test/appTest.js
+++
@@ -1,9 +0,0 @@
-const assert = require("mocha").assert;
-const updateAccount = require("../components/Accounts/Accounts").updateAccount;
-
-describe("App", function() {
-  it("updateAccount should update account with id provided", function() {
-    let result = updateAccount();
-    assert.equal(result, "User has been deleted");
-  });
-});
