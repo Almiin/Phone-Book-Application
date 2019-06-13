8ea2d1994291a069cc3a77cc46a69db6352e9a0a
Almiin <almin.pupalovic1@gmail.com>
2019-06-13 03:04:02 +0200

    first commit

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "/build/index.html")));
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.sendFile(path.join(__dirname + "client/public/index.html"));
 });
 
 app.get("/userAccounts", (req, res) => {
